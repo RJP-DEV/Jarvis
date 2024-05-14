978ff2e7a0434ce3b2ed3bf34be644e92937518b
Raul Jose Perez <68131394+RJP-DEV@users.noreply.github.com>
2024-05-14 02:09:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from transformers import pipeline
+import requests
+import json
+import edge_tts
 import asyncio
 import tempfile
 import os
-import edge_tts
 import inflect
 from huggingface_hub import InferenceClient
 import re
@@ -10,8 +13,10 @@ import time
 from streaming_stt_nemo import Model
 
 number_to_word = inflect.engine()
+
 default_lang = "en"
-engines = {default_lang: Model(default_lang)}
+
+engines = { default_lang: Model(default_lang) }
 
 def transcribe(audio):
     lang = "en"
@@ -20,6 +25,7 @@ def transcribe(audio):
     return text
 
 client1 = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
+
 system_instructions1 = "<s>[SYSTEM] Answer as Real Jarvis JARVIS, Made by 'Tony Stark', Keep conversation very short, clear, friendly and concise. The text provided is a request for a specific type of response from you, the virtual assistant. The request asks you to provide friendly responses as if You are the character Jarvis, made by 'Tony Stark.' The expectation is that I will avoid introductions and start answering the query directly, elaborating on all aspects of the request. As an AI-powered assistant, your task is to generate responses that appear as if they are created by the user and not an AI-powered assistant. [USER]"
 
 def model(text):
@@ -31,23 +37,27 @@ def model(text):
         do_sample=True,
         seed=42,
     )
+    
     formatted_prompt = system_instructions1 + text + "[JARVIS]"
     stream = client1.text_generation(
-        formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False
-    )
+        formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
     output = ""
     for response in stream:
         if not response.token.text == "</s>":
             output += response.token.text
+
     return output
 
 def number_to_words(str):
     words = str.split(' ')
     result = []
+    
     for word in words:
         if( any(char.isdigit() for char in word) ):
             word = number_to_word.number_to_words(word)
+
         result.append(word)
+
     final_result = ' '.join(result).replace('point', '')
     return final_result
 
@@ -72,12 +82,17 @@ MORE = """ ## TRY Other Models
         """
 
 BETA = """ ### Voice Chat"""
+
 FAST = """## Fastest Model"""
+
 Complex = """## Best in Complex Question"""
+
 Detail = """## Best for Detailed Generation or Long Answers"""
 
 base_loaded = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
 client1 = InferenceClient(base_loaded)
+
 system_instructions1 = "[SYSTEM] Answer as Real Jarvis male male, Assistant to Tony Stark, Keep conversation very short, clear, friendly and concise. The text provided is a request for a specific type of response from you, the virtual assistant. The request asks you to provide friendly responses as if You are the character Jarvis, made by 'Tony Stark.' The expectation is that I will avoid introductions and start answering the query directly, elaborating on all aspects of the request. As an AI-powered assistant, your task is to generate responses that appear as if they are created by the user and not an AI-powered assistant. [USER]"
 
 async def generate1(prompt):
@@ -90,17 +105,18 @@ async def generate1(prompt):
     )
     formatted_prompt = system_instructions1 + prompt + "[JARVIS]"
     stream = client1.text_generation(
-        formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=True
-    )
+        formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=True)
     output = ""
     for response in stream:
         if not response.token.text == "</s>":
             output += response.token.text
+
     communicate = edge_tts.Communicate(output)
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
         tmp_path = tmp_file.name
         await communicate.save(tmp_path)
     yield tmp_path
+
 
 st.title("JARVIS")
 
